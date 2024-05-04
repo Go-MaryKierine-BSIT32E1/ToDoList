6d627c0a81a367ccbbe1472ab55927a279a09fd6
Go-MaryKierine-BSIT32E1 <146524749+Go-MaryKierine-BSIT32E1@users.noreply.github.com>
2024-05-04 13:48:01 +0800

    Update ToDoList.cs

--- ToDoList.cs
+++ ToDoList.cs
@@ -1,9 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Xml.Linq;
-using System.Configuration;
 using System.IO;
 using System.ComponentModel;
 
@@ -13,69 +11,52 @@ namespace ToDoListApp
     public class ToDoItem : INotifyPropertyChanged
     {
         private XElement todo;
-        internal XElement ToDo { get { return todo; } }
+
         internal ToDoItem(XElement todo)
         {
             this.todo = todo;
         }
+
         public string CreatedDateTime
         {
             get { return todo.Element("createdatetime").Value; }
             set
             {
                 todo.Element("createdatetime").Value = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("CreatedDateTime"));
-                }
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CreatedDateTime)));
             }
         }
+
         public string Description
         {
             get { return todo.Element("description").Value; }
             set
             {
                 todo.Element("description").Value = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("Description"));
-                }
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Description)));
             }
         }
 
         public string DoneDateTime
         {
-            get { return todo.Element("donedatetime") == null ? "" : todo.Element("donedatetime").Value; }
+            get { return todo.Element("donedatetime")?.Value ?? ""; }
             set
             {
                 if (todo.Element("donedatetime") == null)
                 {
                     todo.Add(new XElement("donedatetime"));
                 }
-                
-                todo.Element("donedatetime").Value = value;
 
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("DoneDateTime"));
-                }
+                todo.Element("donedatetime").Value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DoneDateTime)));
             }
         }
 
-        public bool IsDone { get { return (todo.Element("donedatetime") != null); } }
+        public bool IsDone { get { return todo.Element("donedatetime") != null; } }
 
         public override string ToString()
         {
-            string result;
-            if (DoneDateTime != "")
-            {
-                DateTime doneAt = DateTime.Parse(DoneDateTime);
-                result = String.Format("{0} - Done at: {1:yyyy-MM-dd hh:mm}", Description, doneAt);
-            }
-            else
-            {
-                result = Description;
-            }
+            string result = DoneDateTime != "" ? $"{Description} - Done at: {DateTime.Parse(DoneDateTime):yyyy-MM-dd hh:mm}" : Description;
             return result;
         }
 
@@ -83,100 +64,70 @@ namespace ToDoListApp
     }
 
     [Serializable]
-    public class ToDoList: INotifyPropertyChanged
+    public class ToDoList : INotifyPropertyChanged
     {
-        [NonSerialized]
         private XDocument todoList;
-        [NonSerialized]
         private bool showDoneItems;
-        [NonSerialized]
         private List<ToDoItem> items;
 
-        public List<ToDoItem> Items 
+        public List<ToDoItem> Items
         {
             get
             {
-                if (showDoneItems)
-                {
-                    return items;
-                }
-                else
-                {
-                    return (from i in items
-                            where i.DoneDateTime == ""
-                            select i).ToList();
-                }
+                return showDoneItems ? items : items.Where(i => i.DoneDateTime == "").ToList();
             }
         }
 
-        public bool ShowDoneItems 
-        { 
+        public bool ShowDoneItems
+        {
             get { return showDoneItems; }
-            set 
-            { 
+            set
+            {
                 showDoneItems = value;
-                if (PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("ShowDoneItems"));
-                    PropertyChanged(this, new PropertyChangedEventArgs("Items"));
-                }
-            } 
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShowDoneItems)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
+            }
         }
 
         public ToDoList()
         {
-            AppSettingsReader asr = new AppSettingsReader();
-            string listFileName = (string)asr.GetValue("listFileName", typeof(string));
+            string listFileName = ConfigurationManager.AppSettings["listFileName"];
             if (File.Exists(listFileName))
             {
                 todoList = XDocument.Load(listFileName);
-                items = (from e in todoList.Root.Elements()
-                         select new ToDoItem(e)).ToList();
+                items = todoList.Root.Elements().Select(e => new ToDoItem(e)).ToList();
             }
             else
             {
-                todoList = new XDocument();
-                todoList.Add(new XElement("todolist"));
+                todoList = new XDocument(new XElement("todolist"));
                 items = new List<ToDoItem>();
             }
-
         }
 
-        public void Additem(string description)
+        public void AddItem(string description)
         {
-            XElement item = new XElement("todo");
-            XElement createAt = new XElement("createdatetime");
-            createAt.Value = DateTime.Now.ToString("yyyy-MM-ddThh:mm:ss.ms");
-            item.Add(createAt);
-            XElement desc = new XElement("description");
-            desc.Value = description;
-            item.Add(desc);
+            XElement item = new XElement("todo",
+                new XElement("createdatetime", DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.ms")),
+                new XElement("description", description));
 
             todoList.Root.Add(item);
             items.Add(new ToDoItem(item));
 
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs("Items"));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
         }
 
         public void DeleteItem(ToDoItem item)
         {
             items.Remove(item);
             item.ToDo.Remove();
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs("Items"));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void Save()
         {
-            AppSettingsReader asr = new AppSettingsReader();
-            string listFileName = (string)asr.GetValue("listFileName", typeof(string));
+            string listFileName = ConfigurationManager.AppSettings["listFileName"];
             todoList.Save(listFileName);
         }
     }
